b62073988ad901e0d74ecd2ced06e1bcbb6c57d1
simons1008 <einmorgen@tdmail.de>
2025-07-26 13:51:34 +0200

    Programm hinzugefügt

---
+++ PID_Controller/PID_Basic/PID_Basic.ino
@@ -0,0 +1,73 @@
+/********************************************************
+ * PID Basic Example
+ * Reading analog input 0 to control analog PWM output 3
+ ********************************************************/
+
+#include <PID_v1.h>
+
+#define PIN_INPUT 0
+#define PIN_OUTPUT 3
+
+//Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+
+//Specify the links and initial tuning parameters
+// double Kp=2, Ki=5, Kd=1;
+double Kp=0, Ki=1, Kd=0;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, P_ON_E, DIRECT);
+
+// Setpoint Array
+double mySetpoint[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100, 100, 100, 100, 100,  50, 50, 50, 50, 50,  0};
+
+// Input Array
+double myInput[] =    {60, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 90, 110, 90, 110, 90, 40, 60, 40, 60, 40};
+// double myInput[] =    {50, 0, 0, 0, 0, 0, 0, 0, 0, 0,   0, 100, 100, 100, 100, 100, 50, 50, 50, 50, 50};
+
+// Array Index
+int index = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("PID Test");
+  //initialize the variables we're linked to
+  // Input = analogRead(PIN_INPUT);
+  Input = myInput[index]; 
+
+  // Setpoint = 100;
+  Setpoint = mySetpoint[index]; 
+  
+  // Sample Time in Milliseconds
+  myPID.SetSampleTime(1000);
+  
+  //turn the PID on
+  myPID.SetMode(AUTOMATIC);
+}
+
+void loop()
+{
+  // Input = analogRead(PIN_INPUT);
+  if (myPID.Compute())
+  {
+    // Setpoint
+    Serial.print("Setpoint:");
+    Serial.print(Setpoint); 
+    Serial.print(",");
+    // Input
+    Serial.print("Input:");
+    Serial.print(Input);
+    Serial.print(",");
+    // Output
+    Serial.print("Output:");
+    Serial.println(Output);
+    // neuer Input
+    index ++;
+    if (index > 19)
+      index = 0;
+    Input = myInput[index]; 
+    Setpoint = mySetpoint[index];
+  }
+  // analogWrite(PIN_OUTPUT, Output);
+}
+
+
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 - LED-Ampel mit einer Zustandsmaschine steuern
 - BNO055 Absolute Orientation Sensor: Euler-Winkel lesen
+- PID Regler: Setpoint und Input mit Arrays simuliert
+
 
