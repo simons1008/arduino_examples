dbb1da46dd2e8b503420cb45fdb33b165fee5ab0
simons1008 <einmorgen@tdmail.de>
2025-08-28 16:10:18 +0200

    Programm hinzugefügt

---
+++ PID_Controller/DC_motor_with_encoder/DC_motor_with_encoder.ino
@@ -0,0 +1,129 @@
+/**************************************************************************
+ * The PID Controller controls the position of the "DC Motor with Encoder".  
+ * The target position is a step function or a sinusoidal function  
+ **************************************************************************/
+
+/************************************************************************** 
+ * Quelle: https://github.com/curiores/ArduinoTutorials/tree/main/encoderControl/part4 
+ * Geändert: Zeile 45: Sinusfunktion mit 10-facher Frequenz
+ *           Zeile 49: kp erniedrigt
+ *           Zeile 51: kd erhöht
+ *           Zeile 69: Korrektur: Zielgröße positiv, Rückführung negativ
+ **************************************************************************/
+
+#include <util/atomic.h> // For the ATOMIC_BLOCK macro
+
+#define ENCA 2
+#define ENCB 3
+#define PWM 5
+#define IN2 6
+#define IN1 7
+
+volatile int posi = 0; // specify posi as volatile: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
+long prevT = 0;
+float eprev = 0;
+float eintegral = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ENCA,INPUT);
+  pinMode(ENCB,INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
+  
+  pinMode(PWM,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  
+  Serial.println("target pos");
+}
+
+void loop() {
+
+  // set target position
+  //int target = 1200;
+  //int target = 250*sin(prevT/1e6);
+  int target = 250*sin(prevT/1e5);
+
+  // PID constants
+  // float kp = 1;
+  float kp = 0.7;
+  //float kd = 0.025;
+  float kd = 0.07;
+  float ki = 0.0;
+
+  // time difference
+  long currT = micros();
+  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
+  prevT = currT;
+
+  // Read the position in an atomic block to avoid a potential
+  // misread if the interrupt coincides with this code running
+  // see: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
+  int pos = 0; 
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    pos = posi;
+  }
+  
+  // error
+  //int e = pos - target;
+  int e = target - pos;
+
+  // derivative
+  float dedt = (e-eprev)/(deltaT);
+
+  // integral
+  eintegral = eintegral + e*deltaT;
+
+  // control signal
+  float u = kp*e + kd*dedt + ki*eintegral;
+
+  // motor power
+  float pwr = fabs(u);
+  if( pwr > 255 ){
+    pwr = 255;
+  }
+
+  // motor direction
+  int dir = 1;
+  if(u<0){
+    dir = -1;
+  }
+
+  // signal the motor
+  setMotor(dir,pwr,PWM,IN1,IN2);
+
+
+  // store previous error
+  eprev = e;
+
+  Serial.print(target);
+  Serial.print(" ");
+  Serial.print(pos);
+  Serial.println();
+}
+
+void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
+  analogWrite(pwm,pwmVal);
+  if(dir == 1){
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+  }
+  else if(dir == -1){
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+  }
+  else{
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+  }  
+}
+
+void readEncoder(){
+  int b = digitalRead(ENCB);
+  if(b > 0){
+    posi++;
+  }
+  else{
+    posi--;
+  }
+}
